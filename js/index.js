569deffaba8e328b30b0203d8b870692d1be880a
zeus <lapan40@ukr.net>
2018-04-28 09:36:02 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,49 @@
+$('.owl-carousel').owlCarousel({
+  autoplay: true,
+  autoplayHoverPause: true,
+  loop: true,
+  margin: 20,
+  responsiveClass: true,
+  nav: true,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    568: {
+      items: 2
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 4
+    }
+  }
+})
+$(document).ready(function() {
+  $('.popup-youtube, .popup-text').magnificPopup({
+    disableOn: 320,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: true
+  });
+});
+$(document).ready(function() {
+  $('.popup-text').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#name',
+    callbacks: {
+      beforeOpen: function() {
+        if ($(window).width() < 700) {
+          this.st.focus = false;
+        } else {
+          this.st.focus = '#name';
+        }
+      }
+    }
+  });
+});
